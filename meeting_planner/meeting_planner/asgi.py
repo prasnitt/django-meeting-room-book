47861f54a526a8c0faa96f029024890050c49e49
Prashant Agrawal <prashant.agrawal@fisherpaykel.com>
2021-09-29 11:38:11 +1300

    run black format

--- meeting_planner/meeting_planner/asgi.py
+++ meeting_planner/meeting_planner/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meeting_planner.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meeting_planner.settings")
 
 application = get_asgi_application()
