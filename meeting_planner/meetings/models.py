eb25102e0e96cd5843fa19679dd173bf65aad6ba
Prashant Agrawal <prashant.agrawal@fisherpaykel.com>
2021-09-30 11:59:56 +1300

    add columns in table

--- meeting_planner/meetings/models.py
+++ meeting_planner/meetings/models.py
@@ -1,5 +1,12 @@
+from datetime import time
+
 from django.db import models
 
 class Meeting(models.Model):
     title = models.CharField(max_length=250)
     date = models.DateField()
+    start_time = models.TimeField(default=time(hour=9))
+    duration = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.title} @ {self.start_time} on {self.date} for {self.duration} hour(s)"
