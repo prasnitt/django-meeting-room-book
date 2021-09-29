9d6777cf19fefdf73515d66182e62c62d0cd41c4
Prashant Agrawal <prashant.agrawal@fisherpaykel.com>
2021-09-29 14:05:09 +1300

    add about and date page

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,6 +1,16 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def welcome(request):
     return HttpResponse("Welcome to the Prashant's Meeting Planner!")
+
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
+
+
+def about(request):
+    return HttpResponse("Hey Prashant here. I am learning django via Pluralsight")
