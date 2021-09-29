ffec60daad9298bcdd250e3f7a76f635547ed725
Prashant Agrawal <prashant.agrawal@fisherpaykel.com>
2021-09-30 12:11:25 +1300

    add room table

--- meeting_planner/meetings/models.py
+++ meeting_planner/meetings/models.py
@@ -8,5 +8,13 @@ class Meeting(models.Model):
     start_time = models.TimeField(default=time(hour=9))
     duration = models.IntegerField(default=1)
 
-    def __str__(self):
+    def __str__(self)  -> str:
         return f"{self.title} @ {self.start_time} on {self.date} for {self.duration} hour(s)"
+
+class Room(models.Model):
+    name = models.CharField(max_length=30)
+    floor_number = models.IntegerField(default=1)
+    room_number = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"Room {self.name} Floor Num: {self.floor_number} Room Num: {self.room_number}"
