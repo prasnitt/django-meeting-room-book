ecd60729ff1585b0276b775e23e105a1f622a31d
Prashant Agrawal <prashant.agrawal@fisherpaykel.com>
2021-09-30 16:51:32 +1300

    created an new meeting form

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,4 +1,6 @@
+from django import forms
 from django.shortcuts import render, get_object_or_404
+from django.forms import modelform_factory
 
 from .models import Meeting, Room
 
@@ -11,3 +13,11 @@ def detail(request, id):
 def rooms(request):
     rooms = Room.objects.all()
     return render(request, "meetings/rooms.html", {"rooms": rooms})
+
+
+MeetingForm = modelform_factory(Meeting, exclude=[])
+
+
+def new(request):
+    form = MeetingForm()
+    return render(request, "meetings/new.html", {"form": form})
