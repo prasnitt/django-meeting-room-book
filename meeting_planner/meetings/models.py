694a6237d2cb38eb4ce547ac4bf828f19a9a18dd
Prashant Agrawal <prashant.agrawal@fisherpaykel.com>
2021-09-30 13:49:23 +1300

    connect Meetings to Room db model

--- meeting_planner/meetings/models.py
+++ meeting_planner/meetings/models.py
@@ -2,15 +2,6 @@ from datetime import time
 
 from django.db import models
 
-class Meeting(models.Model):
-    title = models.CharField(max_length=250)
-    date = models.DateField()
-    start_time = models.TimeField(default=time(hour=9))
-    duration = models.IntegerField(default=1)
-
-    def __str__(self)  -> str:
-        return f"{self.title} @ {self.start_time} on {self.date} for {self.duration} hour(s)"
-
 class Room(models.Model):
     name = models.CharField(max_length=30)
     floor_number = models.IntegerField(default=1)
@@ -18,3 +9,14 @@ class Room(models.Model):
 
     def __str__(self) -> str:
         return f"Room {self.name} Floor Num: {self.floor_number} Room Num: {self.room_number}"
+
+class Meeting(models.Model):
+    title = models.CharField(max_length=250)
+    date = models.DateField()
+    start_time = models.TimeField(default=time(hour=9))
+    duration = models.IntegerField(default=1)
+    room = models.ForeignKey(Room,on_delete=models.CASCADE)
+
+    def __str__(self)  -> str:
+        return f"{self.title} @ {self.start_time} on {self.date} for {self.duration} hour(s)"
+
